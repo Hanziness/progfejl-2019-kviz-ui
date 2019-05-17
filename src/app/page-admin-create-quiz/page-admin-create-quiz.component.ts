93881bc3f5805d20184c9c78e04e8baaf75a81d5
Hanziness <hanziness@windowslive.com>
2019-05-17 12:27:50 +0200

    Admin page created (that lets admins author quizzes)

--- src/app/page-admin-create-quiz/page-admin-create-quiz.component.ts
+++ src/app/page-admin-create-quiz/page-admin-create-quiz.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from '../service-quiz.service';
+
+/* 
+  TODO There's a problem with array elements (when deleting and adding new ones)!
+*/ 
 
 @Component({
   selector: 'app-page-admin-create-quiz',
@@ -7,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageAdminCreateQuizComponent implements OnInit {
 
+  questions : Question[] = [];
+  quizName : string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addNewQuestion() {
+    this.questions.push({
+      leiras: '',
+      valaszok: [{
+        nev: '',
+        helyes: true
+      }]
+    });
+  }
+
+  addNewAnswer(question : Question) {
+    question.valaszok.push({
+      nev: '',
+      helyes: false
+    });
+  }
+
+  /**
+   * Turns the questions array into an associative array (ie. object)
+   */
+  getQuizFromQuestions() {
+    let quiz = {};
+
+    this.questions.forEach((value, index) => {
+      quiz['q-' + index] = value;
+    });
+
+    quiz['name'] = this.quizName;
+
+    return quiz;
+  }
+
+  deleteAnswer(question : Question, index : number) {
+    question.valaszok.splice(index, 1);
+  }
+
+  deleteQuestion(index: number) {
+    this.questions.splice(index, 1);
+  }
+
+  debugQuiz() {
+    console.debug(this.getQuizFromQuestions());
   }
 
 }
